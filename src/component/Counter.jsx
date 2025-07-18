1041bebf4a45574c012bcb8ff9c95e3e85a2888b
Karan Singh <karans21.dev@gmail.com>
2025-07-18 10:53:43 +0530

    Multiple of 10 will not print on UI

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -26,18 +26,41 @@ import "./style.css";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    let newCount = count + 1;
+    // console.log("NEWCOUNT", newCount);
+    newCount = count + 1;
+    // console.log("NEWCOUNT AFTER", newCount);
+    if (newCount % 10 === 0) {
+      newCount++;
+    }
+    setCount(newCount);
+  };
+
+  const handleDecrement = () => {
+    let newCount = count - 1;
+    if(newCount % 10 === 0){
+        newCount--;
+    }
+    setCount(newCount);
+  };
+
+    const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="counter-container">
       <h1 className="title">Counter App</h1>
       <p className="count-display">{count}</p>
       <div className="button-group">
-        <button className="btn increment" onClick={() => setCount(count + 1)}>
+        <button className="btn increment" onClick={handleIncrement}>
           Increment
         </button>
-        <button className="btn reset" onClick={() => setCount(0)}>
+        <button className="btn reset" onClick={handleReset}>
           Reset
         </button>
-        <button className="btn decrement" onClick={() => setCount(count - 1)}>
+        <button className="btn decrement" onClick={handleDecrement}>
           Decrement
         </button>
       </div>
